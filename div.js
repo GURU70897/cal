d76e1069a291282468c243a279af0f0ce63a7873
pradeepnaikkumta <pradeepnaikkumta1@gmail.com>
2022-03-10 18:04:35 +0530

    added syuoi0

--- div.js
+++ div.js
@@ -2,6 +2,7 @@ function div()
 {
 var a = document.getElementById("num1").value
 var b = document.getElementById("num2").value
-var result =parseInt(a)+ parseInt(b)
+var result
+result =  a  / b
 document.getElementById("result").value= result
 }
