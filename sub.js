4823dd54ee5ce2a04fe7f5e77d9646e68bfb9cf3
madhu <madhu.honey740@gmail.com>
2022-03-10 16:28:57 +0530

    sub fuction modified

--- sub.js
+++ sub.js
@@ -1,4 +1,9 @@
-var a 
-var b
+function sub()
+{
+var a = document.getElementById("num1").value 
+var b = document.getElementById("num2").value
 var result 
 result = a - b
+
+document.getElementById("result").value = result
+}
