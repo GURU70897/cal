677b42832d324902456ff6c9ff366c280f05a364
GURU70897 <guru70897@gmail.com>
2022-03-10 16:28:49 +0530

    add function modified

--- add.js
+++ add.js
@@ -1,5 +1,9 @@
-var a
-var b
+function add()
+{
+var a = document.getElementById("num1").value 
+var b = document.getElementByid("num2").value
 var result
 
 result = a + b
+document.getElementById("result").value = result
+}
