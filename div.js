0cd3a5a7cda2aaa855510187681fc5fda8b357bf
pradeepnaikkumta <pradeepnaikkumta1@gmail.com>
2022-03-10 17:37:39 +0530

    hello Upadated

--- div.js
+++ div.js
@@ -1,8 +1,8 @@
 function div()
 {
-var a = documeny.getElementById("num1").value
-var b = documeny.getElementById("num2").value
+var a = document.getElementById("num1").value
+var b = document.getElementById("num2").value
 var res
-res=a/b
-documeny.getElementById("result").value= result
+res =  a / b
+document.getElementById("result").value= result
 }
