775771892539bc3a53f53fa7a93b20ff4bf51335
Hssanjay <hssanjay645@gmail.com>
2022-03-10 16:32:52 +0530

    multi edited

--- mult.js
+++ mult.js
@@ -1,4 +1,7 @@
-var a 
-var b
+function multi()
+{var a=document.getElementById("num1").value 
+var b=document.getElementById("num2").value
 var result
 result a*b
+document.getElementById("result").value=result
+}
