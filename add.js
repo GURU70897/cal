425b36a7c4bffa3d879bb27b905fd17954f6a466
GURU70897 <guru70897@gmail.com>
2022-03-10 17:45:26 +0530

    add fully updated

--- add.js
+++ add.js
@@ -2,6 +2,7 @@ function add()
 {
 var a = document.getElementById("num1").value 
 var b = document.getElementByid("num2").value
-var result = a + b
+var result
+result = parseInt( a + b )
 document.getElementById("result").value = result
 }
