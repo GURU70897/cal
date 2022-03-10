732f7a2fd35337ec82a67eaeb1149137940c9b6a
pradeepnaikkumta <pradeepnaikkumta1@gmail.com>
2022-03-10 17:55:05 +0530

    hello nbh

--- div.js
+++ div.js
@@ -2,7 +2,6 @@ function div()
 {
 var a = document.getElementById("num1").value
 var b = document.getElementById("num2").value
-var result
-result =  a / b
+var result =parseInt(a)+ parseInt(b)
 document.getElementById("result").value= result
 }
