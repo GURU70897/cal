521a7d63771fe303b739263d1b655d138b6c8b3e
pradeepnaikkumta <pradeepnaikkumta1@gmail.com>
2022-03-10 16:29:24 +0530

    uPdated

--- div.js
+++ div.js
@@ -1,4 +1,8 @@
-var a
-var b
+function div()
+{
+var a = documeny.getElementById("num1").value
+var b = documeny.getElementById("num2").value
 var res
 res=a/b
+documeny.getElementById("result").value= result
+}
